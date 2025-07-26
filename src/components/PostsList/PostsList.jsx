414441d6ddfe35f94d19258cbaa74bdf8474b711
labattaria <hallucinate322@gmail.com>
2025-07-27 01:54:43 +0300

    refactor PostsList component to use Modal as a wrapper

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewPost from "../NewPost/NewPost";
 import Post from "../Post/Post";
 import styles from './PostsList.module.scss';
+import Modal from "../Modal/Modal";
 
 function PostsList() {
     const [bodyValue, setBodyValue] = useState('');
@@ -16,8 +17,11 @@ function PostsList() {
     }
 
     return (
-        <>
-            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+        <>  
+            <Modal>
+                <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+            </Modal>
+            
             <ul className={styles.posts}>
                 <Post author={authorValue} body={bodyValue} />
                 <Post author="Manuel" body="Learn React with us" />
