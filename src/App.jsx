56c669d82f75b224d3ff08b2b96911e8aa733b97
labattaria <hallucinate322@gmail.com>
2025-07-26 13:13:20 +0300

    update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Niko" body="React Is Awesome" />
+      <Post author="Manuel" body="Learn React with us" />
+    </main>
+  );
 }
 
 export default App;
