ea532b8fd54b8f7c40c77a267785c087dfa9745e
labattaria <hallucinate322@gmail.com>
2025-07-27 01:41:22 +0300

    add useState combined with onChange event handlers

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import NewPost from "../NewPost/NewPost";
 import Post from "../Post/Post";
 import styles from './PostsList.module.scss';
 
 function PostsList() {
+    const [bodyValue, setBodyValue] = useState('');
+    const [authorValue, setAuthorValue] = useState('');
+
+    const bodyChangeHandler = (e) => {
+        setBodyValue(e.target.value);
+    }
+
+    const authorChangeHandler = (e) => {
+        setAuthorValue(e.target.value);
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
             <ul className={styles.posts}>
-                <Post author="Niko" body="React Is Awesome" />
+                <Post author={authorValue} body={bodyValue} />
                 <Post author="Manuel" body="Learn React with us" />
             </ul>
         </>    
