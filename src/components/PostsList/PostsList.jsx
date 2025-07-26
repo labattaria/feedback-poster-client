386d917778015b58df3cd11ac48984cb564d14e1
labattaria <hallucinate322@gmail.com>
2025-07-26 14:40:05 +0300

    refactor PostsList component to include NewPost component

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "../NewPost/NewPost";
 import Post from "../Post/Post";
 import styles from './PostsList.module.scss';
 
 function PostsList() {
     return (
-        <ul className={styles.posts}>
-            <Post author="Niko" body="React Is Awesome" />
-            <Post author="Manuel" body="Learn React with us" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={styles.posts}>
+                <Post author="Niko" body="React Is Awesome" />
+                <Post author="Manuel" body="Learn React with us" />
+            </ul>
+        </>    
     );
 }
 
