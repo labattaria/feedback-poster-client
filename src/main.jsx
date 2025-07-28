bed9967a21e2c4d4a848c1478fbab5d8a3dc946b
labattaria <hallucinate322@gmail.com>
2025-07-28 12:38:44 +0300

    configure and refactor main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './App'
+import Posts from './routes/Posts'
 import './index.css'
-import NewPost from './components/NewPost/NewPost'
+import NewPost from './routes/NewPost/NewPost'
+import RootLayout from './routes/RootLayout'
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/create-post', element: <NewPost /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Posts />,
+        children: [
+          {
+            path: '/create-post',
+            element: <NewPost />,
+          },
+        ]
+      },  
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
