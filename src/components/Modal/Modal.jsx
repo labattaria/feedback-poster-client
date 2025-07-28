722128d28bf485e5f3a1e42afde7a6521a91bab6
labattaria <hallucinate322@gmail.com>
2025-07-28 13:23:27 +0300

    refactor to use useNavigate from react-router-dom

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Modal.module.scss';
     
-function Modal({ children, onClose }) {
+function Modal({ children }) {
+    const navigate = useNavigate();
+
+    const onCloseHandler = () => {
+        navigate('/');
+    }
+
     return (
         <>
-            <div className={styles.backdrop} onClick={onClose} />
+            <div className={styles.backdrop} onClick={onCloseHandler} />
             <dialog open className={styles.modal}>
                 {children}
             </dialog>
